8e3ed0f2e7430fe2e135010a17f0c742c853ab22
portexe <zackazt@gmail.com>
2023-11-25 16:23:58 -0500

    Remove un-necessary class inheritance

--- api/src/app.py
+++ api/src/app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-from sqlalchemy.orm import DeclarativeBase
 from flask_sqlalchemy import SQLAlchemy
-
-class Base(DeclarativeBase):
-  pass
 
 # Initialize the app and DB
 app = Flask(__name__)
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
